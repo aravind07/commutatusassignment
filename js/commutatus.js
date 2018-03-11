9cdf4b4b6e14420f4d172ba66731dd3f98edd1d6
Aravind <aravindmanimaran@gmail.com>
2018-03-12 02:29:42 +0530

    Commutatus AIESEC Page Commit V1

---
+++ js/commutatus.js
@@ -0,0 +1,26 @@
+/**************************
+Commutatus JS File
+Author: Aravind Manimaran
+***************************/
+
+function resizeHeaderOnScroll() {
+	if (screen.width >= 600) {
+		const distanceHeaderY = window.pageYOffset || document.documentElement.scrollTop;
+		shrinkOn = 40;
+		siteTopHeader = document.getElementById('site');
+		siteBodyNav = document.getElementById('siteBodyNav');
+		siteBodyRight = document.getElementById('siteBodyRight');
+
+		if (distanceHeaderY > shrinkOn) {
+		siteTopHeader.classList.add("resizeHeader");
+		siteBodyNav.classList.add("resizeHeader");
+		siteBodyRight.classList.add("resizeHeader");
+		} else {
+		siteTopHeader.classList.remove("resizeHeader");
+		siteBodyNav.classList.remove("resizeHeader");
+		siteBodyRight.classList.remove("resizeHeader");
+		}
+	}
+}
+
+window.addEventListener('scroll', resizeHeaderOnScroll);
